94978323aafe7f1a6d3ed0f19d10ee7ef77e6c1a
donisbr <102358723+donisbr@users.noreply.github.com>
2026-09-25 13:46:42 -0300

    Update filter.py

--- filter.py
+++ filter.py
@@ -4,7 +4,6 @@ SOURCE = "https://cr7v.short.gy/TV"
 OUTPUT = "lista.m3u"
 
 REMOVE = [
- REMOVE = [
     "novelas",
     "pluto tv",
     "pluto series",
@@ -23,28 +22,18 @@ result = []
 skip = False
 
 for line in lines:
-
-    # Identifica o início de um canal
     if line.startswith("#EXTINF:"):
-
-        # Converte para minúsculas para facilitar a comparação
         info = line.lower()
-
-        # Verifica se algum termo proibido aparece na linha inteira
         skip = any(term in info for term in REMOVE)
 
         if not skip:
             result.append(line)
 
-    # URL pertencente ao canal anterior
     elif line.startswith(("http://", "https://")):
-
         if not skip:
             result.append(line)
 
-    # Outras linhas da M3U
     else:
-
         if not skip:
             result.append(line)
 
